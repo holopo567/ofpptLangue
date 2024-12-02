e8a63a2c08eae46675451c4a094edef86f938cfa
jalim <164786918+holopo567@users.noreply.github.com>
2024-12-02 21:07:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import random
 # إعدادات السائق
 def setup_driver():
     options = webdriver.ChromeOptions()
-    #options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
